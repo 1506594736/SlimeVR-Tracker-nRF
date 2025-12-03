5281514ddaf44f253a2738dcfbad6cf17584dc61
Jiting <jiting@jtcat.com>
2025-12-03 17:01:51 +0800

    feat icm45 aux i2cm (wip)

--- src/console.c
+++ src/console.c
@@ -48,7 +48,8 @@ K_THREAD_DEFINE(console_thread_id, 2048, console_thread, NULL, NULL, NULL, 6, 0,
 static const struct device *gpio_dev = DEVICE_DT_GET(DT_NODELABEL(gpio0));
 #endif
 
-#if DT_NODE_HAS_STATUS(DT_NODELABEL(mag), okay)
+#if DT_NODE_HAS_STATUS(DT_NODELABEL(mag), okay) || DT_NODE_HAS_STATUS(DT_NODELABEL(mag_spi), okay)                     \
+	|| DT_NODE_HAS_STATUS(DT_NODELABEL(imu), okay) || DT_NODE_HAS_STATUS(DT_NODELABEL(imu_spi), okay)
 #define SENSOR_MAG_EXISTS true
 #endif
 
